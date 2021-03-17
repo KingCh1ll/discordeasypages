098ba9d6a69ce48dc8ba60df5db7e6c11b921808
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-03-17 16:05:46 -0400

    Version 0.0.4

--- index.js
+++ index.js
@@ -1,49 +1,59 @@
+const Discord = require("discord.js")
+
+/**
+ * 
+ * @param {object} message 
+ * @param {table} pages 
+ * @param {table} emojis 
+ * @returns {object} Current page.
+ */
+
 module.exports = async (message, pages, emojis) => {
-    if (!message){
-        throw new Error("[Discord.js-pages]: Please provide a valid discord message.")
+    if (!message) {
+        throw new Error("[DiscordEasyPages]: Please provide a valid discord message.")
     }
 
-    if (!pages){
-        return new Error("[Discord.js-pages]: Please include pages.")
+    if (!pages) {
+        return new Error("[DiscordEasyPages]: Please include pages.")
     }
 
-    if (!emojis){
+    if (!emojis) {
         emojis = ["⬅", "➡"]
     }
 
-    if (!emojis.length === 2){
-        return new Error(`[Discord.js-pages]: Invalid number of emojis. Expected 2, got ${emojis.length}.`)
+    if (!emojis.length === 2) {
+        return new Error(`[DiscordEasyPages]: Invalid number of emojis. Expected 2, got ${emojis.length}.`)
     }
 
     var PageNumber = 0
     const CurrentPage = await message.channel.send(pages[PageNumber])
 
-    for (const emoji of emojis){
+    for (const emoji of emojis) {
         CurrentPage.edit(pages[PageNumber].setFooter("Please wait until reactions load!"))
 
         try {
             await CurrentPage.react(emoji)
-        } catch(err) {
+        } catch (err) {
             CurrentPage.edit(pages[PageNumber].setFooter("Error occured."))
 
-            return new Error(`[Discord.js-pages]: Error reacting! Are you sure this is a valid emoji?`)
+            return new Error(`[DiscordEasyPages]: Error reacting! Are you sure this is a valid emoji?`)
         }
     }
 
-    CurrentPage.edit(pages[PageNumber].setFooter(`Page ${PageNumber + 1}/${pages.length}`))
+    CurrentPage.edit(pages[PageNumber].setFooter(`${PageNumber + 1}/${pages.length}`))
 
-    const Filter = (reaction, user) => emojis.includes(reaction.emoji.name) && user.id === author.id
+    const Filter = (reaction, user) => emojis.includes(reaction.emoji.name) && user.id === message.author.id
     const ReactionCollector = CurrentPage.createReactionCollector(Filter, {
-        time: 260 * 1000
+        time: pages.length * 2000
     })
 
     ReactionCollector.on("collect", reaction => {
         reaction.users.remove(message.author)
 
-        if (reaction.emoji.name){
-            if (emojis[0]){
+        if (reaction.emoji.name) {
+            if (reaction.emoji.name === emojis[0]) {
                 PageNumber = PageNumber > 0 ? --PageNumber : pages.length - 1
-            } else if (emojis[1]){
+            } else if (reaction.emoji.name === emojis[1]) {
                 PageNumber = PageNumber + 1 < pages.length ? ++PageNumber : 0
             } else {
                 return
@@ -54,7 +64,7 @@ module.exports = async (message, pages, emojis) => {
     })
 
     ReactionCollector.on("end", () => {
-        if (!CurrentPage.deleted){
+        if (!CurrentPage.deleted) {
             CurrentPage.reactions.removeAll()
         }
     })
